afb3c4313b31d53c1ca4ffc827e3f0f3202376cc
Nismit <nismit.dev@gmail.com>
2017-06-16 16:36:21 -0700

    Added Color helper class

--- MySpotsPrototype/MapViewController.swift
+++ MySpotsPrototype/MapViewController.swift
@@ -7,25 +7,29 @@
 //
 
 import UIKit
-import GooglePlaces
+import GoogleMaps
+//import GooglePlaces
 import GooglePlacePicker
 
-class MapViewController: UIViewController, GMSPlacePickerViewControllerDelegate {
+class MapViewController: UIViewController, GMSMapViewDelegate {
     
-//    var locationManager = CLLocationManager()
-//    var currentLocation: CLLocation?
-//    var mapView: GMSMapView!
-//    var placesClient: GMSPlacesClient!
-//    var zoomLevel: Float = 15.0
-//    
-//    // A default location to use when location permission is not granted.
-//    let defaultLocation = CLLocation(latitude: -33.869405, longitude: 151.199)
+    var locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    var mapView: GMSMapView!
+    var placesClient: GMSPlacesClient!
+    var zoomLevel: Float = 15.0
+    
+    // Declare GMSMarker instance at the class level.
+    let infoMarker = GMSMarker()
+    
+    // A default location to use when location permission is not granted.
+    let defaultLocation = CLLocation(latitude: -33.869405, longitude: 151.199)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        //mapInit()
+        mapInit()
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,30 +37,141 @@ class MapViewController: UIViewController, GMSPlacePickerViewControllerDelegate 
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func pickPlace(_ sender: UIButton) {
-        let config = GMSPlacePickerConfig(viewport: nil)
-        let placePicker = GMSPlacePickerViewController(config: config)
-        placePicker.delegate = self
-        present(placePicker, animated: true, completion: nil)
+    func mapInit() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.distanceFilter = 100
+        locationManager.startUpdatingLocation()
+        
+        placesClient = GMSPlacesClient.shared()
+        
+        // Create a map
+        // if user current location can not get, it will be set default position
+        let camera = GMSCameraPosition.camera(withLatitude: defaultLocation.coordinate.latitude,
+                                              longitude: defaultLocation.coordinate.longitude,
+                                              zoom: zoomLevel)
+        mapView = GMSMapView.map(withFrame: view.bounds, camera: camera)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        mapView.settings.myLocationButton = true
+        mapView.isMyLocationEnabled = true
+        mapView.delegate = self
+        
+        // Add the map to the view, hide it until we've got a location update.
+        view.addSubview(mapView)
+        mapView.isHidden = true
+        
     }
     
-    // To receive the results from the place picker 'self' will need to conform to
-    // GMSPlacePickerViewControllerDelegate and implement this code.
-    func placePicker(_ viewController: GMSPlacePickerViewController, didPick place: GMSPlace) {
-        // Dismiss the place picker, as it cannot dismiss itself.
-        viewController.dismiss(animated: true, completion: nil)
+    func mapView(_ mapView:GMSMapView, didTapPOIWithPlaceID placeID:String,
+                 name:String, location:CLLocationCoordinate2D) {
         
-        print("Place name \(place.name)")
-        print("Place address \(String(describing: place.formattedAddress))")
-        print("Place attributions \(String(describing: place.attributions))")
+        // When user tapped a place, picker will be shown up
+        
+        infoMarker.snippet = placeID
+        infoMarker.position = location
+        infoMarker.title = name
+        infoMarker.opacity = 0;
+        infoMarker.infoWindowAnchor.y = 1
+        infoMarker.map = mapView
+        placeInfo(placeID: placeID)
+        mapView.selectedMarker = infoMarker
     }
     
-    func placePickerDidCancel(_ viewController: GMSPlacePickerViewController) {
-        // Dismiss the place picker, as it cannot dismiss itself.
-        viewController.dismiss(animated: true, completion: nil)
-        
-        print("No place selected")
+    func placeInfo(placeID: String) {
+        placesClient.lookUpPlaceID(placeID, callback: { (place, error) -> Void in
+            if let error = error {
+                print("lookup place id query error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let place = place else {
+                print("No place details for \(placeID)")
+                return
+            }
+            
+            print("Place name \(place.name)")
+            print("Place address \(place.formattedAddress)")
+            print("Place placeID \(place.placeID)")
+            print("Place attributions \(place.attributions)")
+            print("Place category \(place.types)")
+            print("Place rating \(place.rating)")
+        })
     }
+    
+//    @IBAction func pickPlace(_ sender: UIButton) {
+//        let config = GMSPlacePickerConfig(viewport: nil)
+//        let placePicker = GMSPlacePickerViewController(config: config)
+//        placePicker.delegate = self
+//        present(placePicker, animated: true, completion: nil)
+//    }
+    
+//    // To receive the results from the place picker 'self' will need to conform to
+//    // GMSPlacePickerViewControllerDelegate and implement this code.
+//    func placePicker(_ viewController: GMSPlacePickerViewController, didPick place: GMSPlace) {
+//        // Dismiss the place picker, as it cannot dismiss itself.
+//        viewController.dismiss(animated: true, completion: nil)
+//        
+//        print("Place name \(place.name)")
+//        print("Place address \(String(describing: place.formattedAddress))")
+//        print("Place attributions \(String(describing: place.attributions))")
+//        print("Place Type \(String(describing: place.types))")
+//    }
+//    
+//    func placePickerDidCancel(_ viewController: GMSPlacePickerViewController) {
+//        // Dismiss the place picker, as it cannot dismiss itself.
+//        viewController.dismiss(animated: true, completion: nil)
+//        
+//        print("No place selected")
+//    }
 
 
+}
+
+// Delegates to handle events for the location manager.
+extension MapViewController: CLLocationManagerDelegate {
+    
+    // Handle incoming location events.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location: CLLocation = locations.last!
+        //print("Location: \(location)")
+        
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
+                                              longitude: location.coordinate.longitude,
+                                              zoom: zoomLevel)
+        
+        if mapView.isHidden {
+            mapView.isHidden = false
+            mapView.camera = camera
+        } else {
+            mapView.animate(to: camera)
+        }
+        
+        //listLikelyPlaces()
+    }
+    
+    // Handle authorization for the location manager.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .restricted:
+            print("Location access was restricted.")
+        case .denied:
+            print("User denied access to location.")
+            // Display the map using the default location.
+            mapView.isHidden = false
+        case .notDetermined:
+            print("Location status not determined.")
+        case .authorizedAlways: fallthrough
+        case .authorizedWhenInUse:
+            print("Location status is OK.")
+        }
+    }
+    
+    // Handle location manager errors.
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManager.stopUpdatingLocation()
+        print("Error: \(error)")
+    }
 }
---
+++ MySpotsPrototype/UIColor+.swift
@@ -0,0 +1,27 @@
+//
+//  UIColor+.swift
+//  MySpotsPrototype
+//
+//  Created by Michinobu Nishimoto on 2017-06-16.
+//  Copyright © 2017 Michinobu Nishimoto. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class func mainLightGreen() -> UIColor {
+        return UIColor( red:0.00, green:0.68, blue:0.69, alpha:1.0 )
+    }
+    
+    class func mainDarkGreen() -> UIColor {
+        return UIColor( red:0.00, green:0.57, blue:0.58, alpha:1.0 )
+    }
+    
+    class func mainDarkBlack() -> UIColor {
+        return UIColor( red:0.22, green:0.22, blue:0.22, alpha:1.0 )
+    }
+    
+    class func mainLightGray() -> UIColor {
+        return UIColor( red:0.22, green:0.22, blue:0.22, alpha:1.0 )
+    }
+}
